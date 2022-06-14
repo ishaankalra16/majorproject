d1dab528ee2c7715ccde2f48660f14cdd41c2eb8
root <root@ip-172-31-0-85.ap-south-1.compute.internal>
2022-06-14 19:44:22 +0000

    added js

---
+++ application/app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+const app = express()
+dotenv.config();
+
+app.get('/:sleepTime',async (req,res) => {
+	const sleepTimeInSeconds = req.params.sleepTime
+
+	const waitForSleepTime = async () => {
+		return new Promise((resolve) => {
+			setTimeout(() => resolve(), sleepTimeInSeconds*1000);
+		})
+
+	}
+
+	await waitForSleepTime();
+
+	res.json({status: "OK"});
+
+});
+
+app.listen(4000, () => {
+	console.log('Server running on port 4000')
+})
